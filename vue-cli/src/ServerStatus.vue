e57e040db8b9633a464ce3c65a80eed2593493de
Tõnis Kasekamp <toniskasekamp@gmail.com>
2018-12-13 22:44:13 +0200

    Add server statuses

---
+++ vue-cli/src/ServerStatus.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <p>Server status: {{ status }}</p>
+    <hr>
+    <button @click="changeStatus">Change status</button>
+  </div>
+</template>
+
+<script>
+  const data = () => ({
+    status: 'critical'
+  });
+
+  export default {
+    data,
+    methods: {
+      changeStatus() {
+        this.status = 'Normal'
+      }
+    }
+  }
+</script>
