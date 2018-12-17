986b204e169f42bfcae4d05796c72a3009ac726a
Tõnis Kasekamp <toniskasekamp@gmail.com>
2018-12-17 21:03:38 +0200

    Add prop validation

--- Section Code (Start)/src/components/UserDetail.vue
+++ Section Code (Start)/src/components/UserDetail.vue
@@ -2,10 +2,26 @@
     <div class="component">
         <h3>You may view the User Details here</h3>
         <p>Many Details</p>
+        <p>User name: {{ switchName() }}</p>
     </div>
 </template>
 
 <script>
+  const props = {
+    name: {
+      type: String,
+      required: true
+    }
+  };
+
+  export default {
+    props,
+    methods: {
+      switchName() {
+        return this.name.split('').reverse().join('');
+      }
+    }
+  }
 </script>
 
 <style scoped>
