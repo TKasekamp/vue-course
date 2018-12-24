4d7689dde22f3c8798da653e6378c6da60c9a2e6
Tõnis Kasekamp <toniskasekamp@gmail.com>
2018-12-24 23:39:45 +0200

    End section 12 with ass 10

--- section12/src/App.vue
+++ section12/src/App.vue
@@ -3,14 +3,47 @@
         <div class="row">
             <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
                 <h1>Directives</h1>
+                <p v-highlight:background.delayed="'green'">Some text</p>
+                <p v-local-highlight.blink="'green'">Some text</p>
 
+                <p v-my-on:click="myAlert('click')">Myon</p>
             </div>
         </div>
     </div>
 </template>
 
 <script>
+    const localHighlight = {
+        bind(el, binding) {
+            const delay = binding.modifiers['delayed'] ? 3000 : 0;
+
+            if (binding.modifiers['blink']) {
+                const mainColor = binding.value;
+                const secondColor = 'blue';
+            } else {
+                setTimeout(() => {
+                    if (binding.arg === 'background') {
+                        el.style.backgroundColor = binding.value;
+                    } else {
+                        el.style.color = binding.value;
+                    }
+                }, delay);
+            }
+        }
+    };
+
+    const myOn = {
+        bind(el, binding) {
+            el.addEventListener(binding.arg, binding.value)
+        }
+    };
+    const myAlert = (a) => () => {
+        alert(a)
+    };
+
     export default {
+        methods: {myAlert},
+        directives: {localHighlight, myOn}
     }
 </script>
 
--- section12/src/main.js
+++ section12/src/main.js
@@ -1,7 +1,23 @@
 import Vue from 'vue'
 import App from './App.vue'
 
+Vue.directive('highlight', {
+    bind(el, binding) {
+        const delay = binding.modifiers['delayed'] ? 3000 : 0 ;
+
+        setTimeout(() => {
+            if (binding.arg === 'background') {
+                el.style.backgroundColor = binding.value;
+            } else {
+                el.style.color = binding.value;
+            }
+        }, delay);
+
+
+    }
+})
+
 new Vue({
-  el: '#app',
-  render: h => h(App)
+    el: '#app',
+    render: h => h(App)
 })
