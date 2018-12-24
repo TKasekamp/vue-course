626334f342498f647e56029f9793dbf677138cec
Tõnis Kasekamp <toniskasekamp@gmail.com>
2018-12-24 21:46:51 +0200

    Assignement 9 end

--- assignement9/src/App.vue
+++ assignement9/src/App.vue
@@ -17,21 +17,65 @@
                     <!-- Exercise 3 -->
                     <!-- Edit the Example from above and create a custom "Full Name" Control -->
                     <!-- which still holds the First Name and Last Name Input Field -->
+                    <h1>Enter details</h1>
+                    <hr>
+                    <full-name v-model="userData"/>
+                    <div class="form-group">
+                        <label for="email">Mail</label>
+                        <input v-model="userData.email"
+                               type="email"
+                               id="email"
+                               class="form-control">
+                    </div>
+                    <div class="form-group">
+                        <label for="password">Password</label>
+                        <input v-model="userData.password"
+                               type="password"
+                               id="password"
+                               class="form-control">
+                    </div>
+                </div>
+
+                <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3 form-group">
+                    <h5>Store data?</h5>
+
+                    <label for="yes">
+                        <input v-model="userData.storeData"
+                               type="radio"
+                               id="yes"
+                               value="yes"> Yes
+                    </label>
+                    <label for="no">
+                        <input v-model="userData.storeData"
+                               type="radio"
+                               id="no"
+                               value="no"> No
+                    </label>
+                </div>
+
+
+            </div>
+            <div class="row">
+                <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
+                    <button @click.prevent="submitted"
+                            class="btn btn-primary">Submit!
+                    </button>
                 </div>
             </div>
+
         </form>
         <hr>
-        <div class="row">
+        <div class="row" v-if="isSubmitted">
             <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
                 <div class="panel panel-default">
                     <div class="panel-heading">
                         <h4>Your Data</h4>
                     </div>
                     <div class="panel-body">
-                        <p>Full Name: </p>
-                        <p>Mail: </p>
-                        <p>Password: </p>
-                        <p>Store in Database?: </p>
+                        <p>Full Name: {{userData.firstName}} {{userData.lastName}}</p>
+                        <p>Mail: {{userData.email}}</p>
+                        <p>Password: {{userData.password}}</p>
+                        <p>Store in Database?: {{userData.storeData}}</p>
                     </div>
                 </div>
             </div>
@@ -40,9 +84,26 @@
 </template>
 
 <script>
+    import FullName from "./FullName.vue";
+
     export default {
+        components: {FullName},
+        data() {
+            return {
+                userData: {
+                    firstName: '',
+                    lastName: '',
+                    email: '',
+                    password: '',
+                    storeData: 'no'
+                },
+                isSubmitted: false
+            }
+        },
+        methods: {
+            submitted() {
+                this.isSubmitted = true
+            }
+        }
     }
 </script>
-
-<style>
-</style>
---
+++ assignement9/src/FullName.vue
@@ -0,0 +1,30 @@
+<template>
+    <div>
+        <div class="form-group">
+            <label for="firstName">First name</label>
+            <input v-model="value.firstName"
+                   type="text"
+                   id="firstName"
+                   class="form-control">
+        </div>
+        <div class="form-group">
+            <label for="firstName">Last name</label>
+            <input v-model="value.lastName"
+                   type="text"
+                   id="lastName"
+                   class="form-control">
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        props: {
+            value: {
+                firstName: String,
+                lastName: String
+            }
+
+        }
+    }
+</script>
