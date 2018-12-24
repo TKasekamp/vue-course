71ed62c9ec46fc710e54895788bf4c5da2fccdb8
Tõnis Kasekamp <toniskasekamp@gmail.com>
2018-12-24 13:33:12 +0200

    Section 9 done

---
+++ section9/src/components/New.vue
@@ -0,0 +1,18 @@
+<template>
+    <div>
+        <h3>New quote</h3>
+    </div>
+</template>
+
+<script>
+    export default {
+        activated() {
+            console.log('activated')
+        },
+        deactivated() {
+            console.log('deactivated')
+        }
+
+    }
+
+</script>
---
+++ section9/src/components/Quote.vue
@@ -0,0 +1,37 @@
+<template>
+    <div>
+        <p>Some quote!</p>
+
+        <div class="title">
+            <slot name="title"/>
+            <span><slot name="subtitle">Default subtitle</slot></span>
+        </div>
+        <hr/>
+        <div>
+            <slot name="content"/>
+        </div>
+        <hr/>
+        <div>
+            <slot/>
+        </div>
+    </div>
+</template>
+
+<script>
+
+
+</script>
+
+<style scoped>
+    div {
+        border: 1px solid #cccccc;
+        box-shadow: 1px 1px 2px black;
+        padding: 30px;
+        margin: 30px auto;
+        text-align: center;
+    }
+
+    .title {
+        font-style: italic;
+    }
+</style>
