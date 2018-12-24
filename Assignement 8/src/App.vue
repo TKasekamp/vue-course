7e9f0eda0ae4b76fa16bc0b97bf8dc4afb484ef5
Tõnis Kasekamp <toniskasekamp@gmail.com>
2018-12-24 13:43:50 +0200

    End ass 8

--- Assignement 8/src/App.vue
+++ Assignement 8/src/App.vue
@@ -3,13 +3,17 @@
         <div class="row">
             <div class="col-xs-12">
                 <br>
-                <button class="btn btn-primary">Load Blue Template</button>
-                <button class="btn btn-success">Load Green Template</button>
-                <button class="btn btn-danger">Load Red Template</button>
+                <button class="btn btn-primary" @click="selectedComponent = 'app-blue'">Load Blue Template</button>
+                <button class="btn btn-success" @click="selectedComponent = 'app-green'">Load Green Template</button>
+                <button class="btn btn-danger" @click="selectedComponent = 'app-red'">Load Red Template</button>
                 <hr>
-                <app-blue></app-blue>
-                <app-green></app-green>
-                <app-red></app-red>
+                <component :is="selectedComponent">
+                    <h2>Component slot</h2>
+                </component>
+                <hr/>
+                <app-blue><h2>I am Blue!</h2></app-blue>
+                <app-green><h2>I am Green!</h2></app-green>
+                <app-red><h2>I am Red!</h2></app-red>
             </div>
         </div>
     </div>
@@ -21,6 +25,11 @@
     import Red from './components/Red.vue';
 
     export default {
+        data: function (){
+            return {
+                selectedComponent: 'app-blue'
+            }
+        },
         components: {
             appBlue: Blue,
             appGreen: Green,
