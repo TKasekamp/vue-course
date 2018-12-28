7d85dfefb867e63dcf68d54a0b5e374cbb6bd8dd
Tõnis Kasekamp <toniskasekamp@gmail.com>
2018-12-28 15:27:40 +0200

    End section 13

--- section13/src/App.vue
+++ section13/src/App.vue
@@ -3,17 +3,37 @@
         <div class="row">
             <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
                 <h1>Filters & Mixins</h1>
-
+                <p>{{text | toUppercase | toLowercase }}</p>
+                <hr>
+                <input type="text" v-model="filterText">
+                <ul>
+                    <li v-for="f in filteredFruits">{{f}}</li>
+                </ul>
+                <app-list/>
             </div>
         </div>
     </div>
 </template>
 
 <script>
+    import List from "./List.vue";
+    import {fruitMixin} from "./fruitMixin";
+
     export default {
+        data() {
+            return {
+                text: 'Hello',
+            }
+        },
+        filters: {
+            toUppercase(value) {
+                return value.toUpperCase();
+            }
+        },
+
+        components: {
+            appList: List
+        },
+        mixins: [fruitMixin]
     }
 </script>
-
-<style>
-
-</style>
---
+++ section13/src/List.vue
@@ -0,0 +1,18 @@
+<template>
+    <div>
+        <input type="text" v-model="filterText">
+        <ul>
+            <li v-for="f in filteredFruits">{{f}}</li>
+        </ul>
+
+    </div>
+</template>
+
+<script>
+    import {fruitMixin} from "./fruitMixin";
+
+    export default {
+        mixins: [fruitMixin]
+    }
+</script>
+
