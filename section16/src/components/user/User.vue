751a532b645972f426b26f9722992e98186da0ab
Tõnis Kasekamp <toniskasekamp@gmail.com>
2018-12-31 21:20:06 +0200

    Add routing

--- section16/src/components/user/User.vue
+++ section16/src/components/user/User.vue
@@ -1,4 +1,19 @@
 <template>
-    <h1>The User Page</h1>
-
+    <div>
+        <h1>The User Page</h1>
+        <hr>
+        <p>{{id}}</p>
+        <button @click="navigateHome" class="btn btn-danger">Go home</button>
+        <hr>
+        <router-view></router-view>
+    </div>
 </template>
+<script>
+  export default {
+    methods: {
+      navigateHome() {
+        this.$router.push('/')
+      }
+    }
+  }
+</script>
--- section16/src/components/user/UserDetail.vue
+++ section16/src/components/user/UserDetail.vue
@@ -1,3 +1,9 @@
 <template>
-    <h3>Some User Details</h3>
+    <div>
+        <h3>Some User Details</h3>
+        <p>User id {{$route.params.id}}</p>
+        <router-link tag="button" class="btn btn-default" :to="`/user/${$route.params.id}/edit`">Edit</router-link>
+        <router-link tag="button" class="btn btn-default" :to="{name: 'userEdit', params: {id: $route.params.id}}">Edit</router-link>
+    </div>
+
 </template>
