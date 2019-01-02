0e63f00029ee15ad8ea16769a8d556a1ffc09962
Tõnis Kasekamp <toniskasekamp@gmail.com>
2019-01-02 22:16:06 +0200

    Add routes

--- section18/src/App.vue
+++ section18/src/App.vue
@@ -1,14 +1,18 @@
 <template>
     <div class="container">
-
+        <appHeader></appHeader>
+        <router-view></router-view>
     </div>
 </template>
 
 <script>
+  import Header from './components/Header.vue';
 
-    export default {
-
+  export default {
+    components: {
+      appHeader: Header
     }
+  }
 </script>
 
 <style>
